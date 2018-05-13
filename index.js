7d180bb8f6d81d4ec4f5471b637d065cb2a9e2b5
Willis Plummer <willisplummer@gmail.com>
2018-05-13 13:56:53 -0400

    working reducer using callbacks

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
-console.log('first commit');
+const Increment = { type: 'INCREMENT' }
+const Decrement = { type: 'DECREMENT' }
+
+const counter = (state = 0, action) => {
+  switch (action.type) {
+  case 'INCREMENT':
+    return state + 1
+  case 'DECREMENT':
+    return state - 1
+  default:
+    return state
+  }
+}
+
+const createStore = (r) => {
+  const reducer = r
+  let state = reducer(undefined, { type: 'INITIALIZE' })
+  let subscriptions = []
+  return {
+    dispatch: (action) => { state = reducer(state, action); subscriptions.forEach(cb => cb(state)) },
+    getState: () => state,
+    subscribe: (callback) => { subscriptions.push(callback) }
+  }
+}
+
+const store = createStore(counter)
+store.subscribe(console.log)
+
+store.dispatch(Increment)
+store.dispatch(Increment)
+store.dispatch(Decrement)
